46ffb0829f581fa74de956913085bf0f90e048e2
keemsy <phzzz@naver.com>
2023-01-10 22:55:45 +0900

    Fix: remove to project to prepare for project initialization

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Order.java
+++
@@ -1,7 +0,0 @@
-//package com.food.ordering.system.order.service.domain.entity;
-////
-//import com.food.ordering.system.domain.entity.AggregateRoot;
-//// 주문 프로세스의 집계 루트가 된다.
-//public class Order extends AggregateRoot{
-//
-//}
