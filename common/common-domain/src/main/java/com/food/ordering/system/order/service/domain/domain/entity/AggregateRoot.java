f54c84d77d953eff7dc3b1567f58ff9226dca9ad
keemsy <phzzz@naver.com>
2023-01-11 21:29:05 +0900

    fix: fix directory path

--- common/common-domain/src/main/java/com/food/ordering/system/order/service/domain/domain/entity/AggregateRoot.java
+++
@@ -1,5 +0,0 @@
-package com.food.ordering.system.order.service.domain.domain.entity;
-
-public abstract class AggregateRoot<ID> extends BaseEntity<ID>{
-
-}
--- common/common-domain/src/main/java/com/food/ordering/system/order/service/domain/domain/event/DomainEvent.java
+++
@@ -1,4 +0,0 @@
-package com.food.ordering.system.order.service.domain.domain.event;
-
-public interface DomainEvent<T> {
-}
