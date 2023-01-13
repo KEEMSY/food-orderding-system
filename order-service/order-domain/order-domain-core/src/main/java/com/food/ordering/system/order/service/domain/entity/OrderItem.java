98ad0ec0761293164afd53f6eb833957e2faf35f
keemsy <phzzz@naver.com>
2023-01-13 11:29:57 +0900

    add: add Order entity implementtation methods

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/OrderItem.java
@@ -12,6 +12,11 @@ public class OrderItem extends BaseEntity<OrderItemId> {
     private final Money price;
     private final Money subTotal;
 
+    boolean isPriceValid() {
+        return price.isGreaterThanZero() &&
+                price.equals(product.getPrice()) &&
+                price.multiply(quantity).equals(subTotal);
+    }
     private OrderItem(Builder builder) {
         super.setId(builder.orderItemId);
         orderId = builder.orderId;
@@ -21,6 +26,10 @@ public class OrderItem extends BaseEntity<OrderItemId> {
         subTotal = builder.subTotal;
     }
 
+    void initializeOrderItem(OrderId orderId, OrderItemId orderItemId) {
+        this.orderId = orderId;
+        super.setId(orderItemId);
+    }
 
     public OrderId getOrderId() {
         return orderId;
