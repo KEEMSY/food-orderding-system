016812c07f93755f9161e766f8a0249fb2efbceb
keemsy <phzzz@naver.com>
2023-01-14 10:14:51 +0900

    feat: implement Domain Events in Order Service domain layer

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/OrderDomainServiceImpl.java
@@ -0,0 +1,46 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.entity.Restaurant;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
+import com.food.ordering.system.order.service.domain.event.OrderCreateEvent;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class OrderDomainServiceImpl implements OrderDomainService{
+    @Override
+    public OrderCreateEvent validateAndInitiateOrder(Order order, Restaurant restaurant) {
+        validateRestaurant(restaurant);
+        setOrderProductInformation(order,restaurant);
+        order.validateOrder();
+        order.initializeOrder();
+        return new OrderCreateEvent(order, ZonedDateTime.now(ZoneId.of("UTC")));
+    }
+
+    @Override
+    public OrderPaidEvent payOrder(Order order) {
+        return null;
+    }
+
+    @Override
+    public void approveOrder(Order order) {
+
+    }
+
+    @Override
+    public OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages) {
+        return null;
+    }
+
+    @Override
+    public void cancelOrder(Order order, List<String> failureMessages) {
+
+    }
+    private void validateRestaurant(Restaurant restaurant) {
+    }
+    private void setOrderProductInformation(Order order, Restaurant restaurant) {
+    }
+}
