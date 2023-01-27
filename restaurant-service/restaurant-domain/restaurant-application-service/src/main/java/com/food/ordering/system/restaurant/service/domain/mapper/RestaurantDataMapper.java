c7121ae5efbd633785bcfd427dfffa33aac87e19
keemsy <phzzz@naver.com>
2023-01-27 20:16:25 +0900

    Add: add application Service domain module to add Mapper, DTO and Ports

---
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/mapper/RestaurantDataMapper.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.restaurant.service.domain.mapper;
+
+
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Component
+public class RestaurantDataMapper {
+
+}
